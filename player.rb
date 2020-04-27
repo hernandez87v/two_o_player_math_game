7c2a228e35584f9f5c35aae18e8da7b2831d9712
hernandez87v <hernandez87v@hotmail.com>
2020-04-27 14:22:33 -0700

    Added question with random number generator instead of questions.rb

--- player.rb
+++ player.rb
@@ -4,12 +4,18 @@ class Player
     attr_accessor :name, :lives
 
     def  initialize(n)
-        @name = n
-        @lives = 3
+        self.name = n
+        self.lives = 3
     end
 
     def rando_num
         rand(20)
+    end
+
+    def question
+        self.num_one = rando_num
+        self.num_two = rando_num
+        puts "#{self.name}: What does #{self.num_one} plus #{self.num_two} equal?"
     end
 
 end
@@ -17,3 +23,4 @@ end
 vlad = Player.new("Vlad")
 puts vlad.name
 puts vlad.lives
+puts question.inspect
